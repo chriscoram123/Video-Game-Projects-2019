889996a47138da6cb7a3837df95a242da5ad8706
Christopher Coram <ultraartist15@gmail.com>
2019-03-10 10:38:04 -0400

    Add files via upload

---
+++ HyperDrive/DEMO/Scripts/InstantiateProjectileOnTrigger2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBulletInstantiate : MonoBehaviour {
+    public Rigidbody Bullet;
+    public float speed;
+    public float speed2;
+    public float destroyTime;
+    public Rigidbody barrelEnd;
+
+    void OnTriggerEnter(Collider other) {
+        if(other.CompareTag("Controller")) {
+            Instantiate(Bullet, barrelEnd.position, barrelEnd.rotation);
+            Bullet.AddForce(speed * Time.deltaTime, speed2 * Time.deltaTime, 0);
+            Destroy(Bullet, destroyTime);
+            Update();
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        Debug.Log("BULLET HAS BEEN LAUNCHED AND WILL BE DESTROYED WITHIN X AMOUNT OF SECONDS");
+    }
+}
