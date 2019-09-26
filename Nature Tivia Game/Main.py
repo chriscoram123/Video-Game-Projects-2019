a9778664450b20768b9abe70881def52e2dd99f5
Christopher Coram <ultraartist15@gmail.com>
2019-09-26 19:38:58 -0400

    Update Main.py

--- Nature Tivia Game/Main.py
+++ Nature Tivia Game/Main.py
@@ -13,10 +13,14 @@ from LevelThree import *
 #####################
 ##### VARIABLES #####
 #####################
+# Imported classes
+classOne = q_One()
+classTwo = q_Two()
+classThree = q_Three()
 
 # Main title display 
-top_Layer =  "1001000100100010001000"
-lower_Layer = "001001000100010001001"
+top_Layer =  "-----------------------"
+lower_Layer = "----------------------"
 game_Title = "NATURE TRIVA GAME"
 developer = "by Christopher S Coram"
 
@@ -24,13 +28,35 @@ developer = "by Christopher S Coram"
 game_Running = True
 
 
-######################
-##### WHILE LOOP #####
-######################
+################
+##### GAME #####
+################
+# Video game title display
 while game_Running:
-  # Video game title display
   print(top_Layer)
   print(game_Title)
   print(developer)
+  print("INSTRUCTIONS: When entering answers do not space out the first letter from the last letter in HINT.")
+  print("Also, always capitalize the first letter of your answer.")
   print(lower_Layer)
 
+  # Running LevelOne questions
+  classOne.round_Questions()
+  print(lower_Layer)
+  # Running LevelTwo questions
+  classTwo.round_Questions_Two()
+  print(lower_Layer)
+  # Running LevelThree questions
+  classThree.round_Questions_Three()
+  print(lower_Layer)
+
+  # Decision if / else statement
+  if input("You gained a total of 150 points! Now that the game is finished, would you like to exit the game or play again?(yes/no)") == "Yes":
+    print("Continue loop")
+  elif input("You gained a total of 150 points! Now that the game is finished, would you like to exit the game or play again?(yes/no)") == "yes":
+    print("Continue loop")
+  elif input("You gained a total of 150 points! Now that the game is finished, would you like to exit the game or play again?(yes/no)") == "No":
+    sys.exit()
+  elif input("You gained a total of 150 points! Now that the game is finished, would you like to exit the game or play again?(yes/no)") == "no":
+    sys.exit()
+    
